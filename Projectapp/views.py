d1748818c1678b7009c64ebc1cffb46b5beece7b
richie <oftsurich@gmail.com>
2023-12-18 12:44:45 +0000

    added permission

--- Projectapp/views.py
+++ Projectapp/views.py
@@ -1,12 +1,17 @@
 from rest_framework import generics
 from .models import CustomUserProfile
 from .serializers import CustomUserProfileSerializer
-
+from rest_framework.authentication import TokenAuthentication
+from Projectapp import permission
 # Create your views here.
 class CustomUserProfileCreateAPIView(generics.ListCreateAPIView):
     queryset = CustomUserProfile.objects.all()
     serializer_class = CustomUserProfileSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permission.OwnProfile,)
 
 class CustomUserProfileRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = CustomUserProfile.objects.all()
     serializer_class = CustomUserProfileSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permission.OwnProfile,)
