d3daa9438e46060d0bc1a1f612fb506e9c427c8c
richie <oftsurich@gmail.com>
2023-12-18 12:50:32 +0000

    Added searching to user Profile

--- Projectapp/views.py
+++ Projectapp/views.py
@@ -3,15 +3,20 @@ from .models import CustomUserProfile
 from .serializers import CustomUserProfileSerializer
 from rest_framework.authentication import TokenAuthentication
 from Projectapp import permission
+from rest_framework import filters
 # Create your views here.
 class CustomUserProfileCreateAPIView(generics.ListCreateAPIView):
     queryset = CustomUserProfile.objects.all()
     serializer_class = CustomUserProfileSerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permission.OwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email')
 
 class CustomUserProfileRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = CustomUserProfile.objects.all()
     serializer_class = CustomUserProfileSerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permission.OwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email')
