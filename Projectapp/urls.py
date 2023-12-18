a3463e7d78100beeb22c8d0030ab301ec314b8a8
richie <oftsurich@gmail.com>
2023-12-18 13:07:37 +0000

    Added Login Api

--- Projectapp/urls.py
+++ Projectapp/urls.py
@@ -1,8 +1,10 @@
+from .views import UserLoginApiView
 from django.urls import path
 from .views import CustomUserProfileCreateAPIView,CustomUserProfileRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('create/', CustomUserProfileCreateAPIView.as_view(), name='customuserprofile-create'),
     path('create/<int:pk>/',CustomUserProfileRetrieveUpdateDestroyView.as_view(), name='customuserprofile-retrieve-update-destroy'),
+    path('login/',UserLoginApiView.as_view()),
     # Add other URL patterns if needed
 ]
